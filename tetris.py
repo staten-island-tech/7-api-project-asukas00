2354c247ffc60d9127741cded4df59e6e68383ad
asukas00 <jessere2025@gmail.com>
2025-12-08 09:52:51 -0500

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -11,31 +11,34 @@ chessplayerlabel = Label(window, font="Arial, 12")
 
 
 def get_user():
-
-  
     try:
-        response = requests.get(f"https://api.chess.com/pub/player/{username}")
+        chess = enteruser.get() 
+        data = requests.get(f"https://api.chess.com/pub/player/{chess.capitalize}")
         
-        if response.status_code == 200:
-            data = response.json()
 
+        if data.status_code == 200:
+            display_text = (
+                f"avatar: {data['avatar']}",
+                f"name:  {data['name']}",
+                f"username: {data['username']}"
+                f"League: {data['league']}",
+                f"Location: {data['location']},"
+                f"Url: {data['url']}"
+            )
+            chessplayerlabel.config(text= display_text , bg = "black")
 
-
-
-
-
-
-
-
-    if username == "":
-        chessplayerlabel.config(text="Please enter a username.", fg="red")
-        return
-
-    except:
         
+        if not data.status_code == 200:
+            error_message = f"Error: No player named '{chess.capitalize()}' found. Try again."
+            chessplayerlabel.config(text=error_message, fg="red")
+
+
+    except requests.ConnectionError:
+       chessplayerlabel.config(text="Connection Error: Could not connect to the API.", fg="red")
 
 
 submit_button.config(command=get_user)
+
 
 enteruser.pack(pady=10)
 submit_button.pack(pady=10)
